08b9992fb11b1ef193f6838e4bd817080e155018
tyson-tyler <benword25@gmail.com>
2024-05-13 07:27:28 +0530

    sfsfsf

---
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,15 @@
+import { SignIn } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
+
+export default function Page() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <SignIn
+        path="/sign-in"
+        appearance={{
+          baseTheme: dark,
+        }}
+      />
+    </div>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +18,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <ClerkProvider>
+        <body
+          className={`${inter.className} dark:bg-black dark:text-white bg-white text-black`}
+        >
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </body>
+      </ClerkProvider>
     </html>
   );
 }
